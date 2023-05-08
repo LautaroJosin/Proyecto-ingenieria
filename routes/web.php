fe0e5ca75486bc1783886992928d685ef50ee24e
Lautaro Castro <lautaro.castro.est@gmail.com>
2023-05-07 21:29:52 -0300

    Modificación(web.php): Se agrego las rutas para la implementación de la gestión de perros

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(DogController::class)->group(function () {
+    Route::prefix('/usuario/perros')->group(function () {
+        Route::name('dog.')->group(function () {
+            Route::get('', 'index')->name('');
+            Route::post('', 'store')->name('store');
+            Route::get('/crear', 'create')->name('create');
+            Route::delete('/borrar/{id}', 'destroy')->name('destroy');
+            Route::get('/libreta/{id}', 'showHealthBook')->name('showHealthBook'); //Después se implementará. Se usará un singleton
+        });
+    });
 });
