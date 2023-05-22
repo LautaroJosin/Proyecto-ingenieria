41d965da33391920332cc092079de9d76b6abd56
lauj32 <ljosinsaller@gmail.com>
2023-05-21 22:36:04 -0300

    Modificacion: borre todo lo relacionado a dashboard. Tanto la vista como referencias a la vista
    Modificacion: cambie la vista para editar el perfil de un usuario y ahora se hereda de layout master.
    
    Modifiacion: layout master ahora es la vista de la que heredan todas las vistas de usuarios logueados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,9 +15,7 @@
 
         <div class="flex justify-end text-center p-10">
             @if (Route::has('login'))
-                @auth
-                    {{-- <a href="{{ url('/dashboard') }}" class="text-main-menu font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 text-xl">Dashboard</a> --}}
-                    
+                @auth 
                     @can('create user') 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}" class="text-main-menu ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar usuario</a>
--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-/*
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
