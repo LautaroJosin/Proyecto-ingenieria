a4ce0ed88074a80a8b84fc5c9d54ab4a3cfae1d1
lauj32 <ljosinsaller@gmail.com>
2023-05-21 21:41:24 -0300

    Muevo un poco el boton del usuario en el menu navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
                     <x-dropdown>
 
                         <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-main-menu bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            <button class="mr-20 inline-flex items-center px-3 py-2 border-2 border-transparent text-sm leading-4 font-medium rounded-md text-main-menu bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                 <div>{{ Auth::user()->name }}</div>
 
                                     <div class="ml-1">
