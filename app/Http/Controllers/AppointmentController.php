adb8b2dd35320dc271a3d119baf833204e2530f6
Lautaro Castro <lautaro.castro.est@gmail.com>
2023-06-11 10:21:29 -0300

    Arreglo: Se agrego middleware por autenticación a AppointmentController

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function filter(Request $request)
     {
         if ($request->filled('state')) {
