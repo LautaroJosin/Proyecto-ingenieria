2060ca5a9b6b263ed5a9f38d62a2e0f492ecfd24
Lautaro Castro <lautaro.castro.est@gmail.com>
2023-06-17 21:23:14 -0300

    Arreglo: Se agrego campo start_date a migration donation_campaign

--- database/migrations/2023_06_17_215816_donation_campaigns.php
+++ database/migrations/2023_06_17_215816_donation_campaigns.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('donation_campaigns', function(Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('start_date');
             $table->date('end_date');
             $table->integer('fundraising_goal');
             $table->string('description');
