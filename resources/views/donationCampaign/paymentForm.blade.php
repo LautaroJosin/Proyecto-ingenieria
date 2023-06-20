7b66b89463644d819431bc13ba9254024c0fb642
lauj32 <ljosinsaller@gmail.com>
2023-06-19 22:11:30 -0300

    Fix: ahora la columna 'current_fundraised' se inicializa en 0 al guardar una campaña

--- resources/views/donationCampaign/paymentForm.blade.php
+++ resources/views/donationCampaign/paymentForm.blade.php
@@ -19,19 +19,20 @@
         @csrf
         @method('PUT')
 
-            <div class="grid grid-cols-q grid-rows-5 gap-5 mr-20 text-2xl">
+            <div class="grid grid-cols-q grid-rows-6 gap-5 mr-20 text-2xl">
                 <label>Monto:</label>
                 <label>Tipo de tarjeta:</label>
 
+                <label>Titular de la tarjeta:</label>
                 <label>Numero de tarjeta:</label>
                 <label>CVV:</label>
                 <label>Fecha de expiración:</label>
 
             </div>
 
-            <div class="grid grid-cols-q grid-rows-5 gap-5 w-10 text-black font-normal">
+            <div class="grid grid-cols-q grid-rows-6 gap-5 w-10 text-black font-normal">
 
-            	<input type="number" name="amount" class="form-control" pattern="[A-Za-z ]+" required>
+            	<input type="number" name="amount" class="form-control" required min="1">
 			
 	            <select name="card_type" required>
 	            	<option value="">Seleccione una tarjeta</option>
@@ -39,9 +40,11 @@
 	                <option value="D">Tarjeta de credito </option>
 	            </select>
 
+                <input type="text" name="cardholder" class="form-control" pattern="[A-Za-z ]+" required>
+
 	            <input type="number" name="card_number" class="form-control" required>
 
-	            <input type="number" name="cvv" class="form-control" required>
+	            <input type="number" name="cvv" class="form-control" required min="100" max="999">
 
 	            <input type="date" name="expiration_date" class="form-control" min="{{  date('Y-m-d') }}" required>
 
