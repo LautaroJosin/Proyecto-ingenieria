96c1504467de704fcd192a339883f990d17e58ea
lauj32 <ljosinsaller@gmail.com>
2023-07-04 22:32:51 -0300

    Agregacion: ahora en el perfil de un usuario (que no sea admin) se pueden ver los creditos

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -14,10 +14,16 @@
 
     @include('layouts.navigation')
 
-    <header class="bg-white shadow">
+    <header class="bg-white shadow mt-10">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Profile') }}
+
+                @role('user')
+                <br>
+                <br>
+                    Mis creditos : {{ Auth::user()->credits }}
+                @endrole
             </h2>
         </div>
     </header>
